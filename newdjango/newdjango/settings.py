7bb9162ebf874221e0bbbc9f9ddc8599094f574a
Champak's World <champaksworld@gmail.com>
2022-05-16 09:49:02 +0530

    VSJ Polls tables migrated

--- newdjango/newdjango/settings.py
+++ newdjango/newdjango/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vsjpolls.apps.VsjpollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
