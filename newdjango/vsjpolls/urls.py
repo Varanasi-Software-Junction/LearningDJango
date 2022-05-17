dc9b7bd77b6c18d797bfe48fc7593e27b1a40a23
Champak's World <champaksworld@gmail.com>
2022-05-17 09:39:01 +0530

    Added Editing to Admin

--- newdjango/vsjpolls/urls.py
+++ newdjango/vsjpolls/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
+
+    path('<int:question_id>/results/', views.results),
+
+    path('<int:question_id>/vote/', views.vote),
 ]
